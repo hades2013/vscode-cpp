db98551ac4d688add3e4bde8fd0a9eb86cd958c8
hades <hades2013ac@gmail.com>
2018-04-09 15:10:55 +0800

    add patition_copy algorithm

---
+++ patition_copy.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+ 
+int main()
+{
+    int arr [10] = {1,2,3,4,5,6,7,8,9,10};
+    int true_arr [5] = {0};
+    int false_arr [5] = {0};
+ 
+    std::partition_copy(std::begin(arr), std::end(arr), std::begin(true_arr), std::begin(false_arr),
+                        [] (int i) {return i > 5;});
+ 
+    std::cout << "true_arr: ";
+    for (auto x : true_arr) {
+        std::cout << x << ' ';
+    }
+    std::cout << '\n'; 
+ 
+    std::cout << "false_arr: ";
+    for (auto x : false_arr) {
+        std::cout << x << ' ';
+    }
+    std::cout << '\n'; 
+ 
+    return 0;
+ 
+}

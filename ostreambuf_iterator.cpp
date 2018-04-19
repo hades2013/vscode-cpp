8d5fc253c427ae1efc86fad3d15922890c73195b
hades <hades2013ac@gmail.com>
2018-04-19 10:28:45 +0800

    add ostreambuf_iterator iterator

---
+++ ostreambuf_iterator.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+ 
+int main()
+{
+    std::string s = "This is an example\n";
+    std::copy(s.begin(), s.end(), std::ostreambuf_iterator<char>(std::cout));
+}

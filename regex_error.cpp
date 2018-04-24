1c753b0b96335525401f1df706437f54b17d576e
hades <hades2013ac@gmail.com>
2018-04-24 16:39:47 +0800

    add regex_error iterator

---
+++ regex_error.cpp
@@ -0,0 +1,17 @@
+
+#include <regex>
+#include <iostream>
+ 
+int main()
+{
+    try {
+        std::regex re("[a-b][a");
+    } 
+ 
+    catch (const std::regex_error& e) {
+        std::cout << "regex_error caught: " << e.what() << '\n';
+        if (e.code() == std::regex_constants::error_brack) {
+            std::cout << "The code was error_brack\n";
+        }
+    }
+}
